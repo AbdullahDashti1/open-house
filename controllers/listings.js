29a4d693a6dd95fd998c721c07ce946c84b5b37a
Abdulla Dashti <abdullamustafadashti.0@gmail.com>
2025-09-20 12:55:11 +0300

    unfavorite function

--- controllers/listings.js
+++ controllers/listings.js
@@ -86,4 +86,16 @@ router.delete('/:listingId', async (req, res) => {
   }
 });
 
+router.delete('/:listingId/favorited-by/:userId', async (req, res) => {
+  try {
+    await Listing.findByIdAndUpdate(req.params.listingId, {
+      $pull: { favoritedByUsers: req.params.userId },
+    });
+    res.redirect(`/listings/${req.params.listingId}`);
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
 module.exports = router;
